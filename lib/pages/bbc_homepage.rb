f7ebe9d164cb522117579994d86ec34b32810849
Ruben Pinto <rpinto@spartaglobal.com>
2018-12-05 09:23:15 +0000

    Relevant comments added to the methods

--- lib/pages/bbc_homepage.rb
+++ lib/pages/bbc_homepage.rb
@@ -5,14 +5,17 @@ class BbcHomepage
     HOMEPAGE = 'https://www.bbc.co.uk'
     SIGN_IN_LINK_ID = '#idcta-link' #id for the sign in button on BBC website
 
+    # visits the bbc homepage
     def visit_homepage
         visit(HOMEPAGE)
     end
 
+    # finds the sign in button element on the page
     def sign_in_link 
         find(SIGN_IN_LINK_ID)
     end
 
+    #clicks on the sign in page
     def click_sign_in_link
         find(SIGN_IN_LINK_ID).click
     end
--- lib/pages/bbc_sign_in_page.rb
+++ lib/pages/bbc_sign_in_page.rb
@@ -8,34 +8,42 @@ class BbcSignInPage
     USERNAME_INPUT_ID = "user-identifier-input"
     PASSWORD_INPUT_ID = "password-input"
 
+    # visits the sign in page
     def visit_sign_in_page
         visit(SIGN_IN_PAGE_URL)
     end
 
+    # click on the sign in button that will take to the sign in form
     def click_sign_in_button
         find(SIGN_IN_BUTTON_ID).click
     end
 
+    #fills in the username input box with a username that does not exist
     def input_invalid_username
         fill_in(USERNAME_INPUT_ID, :with => 'Ruben12ddf')
     end
 
+    #fills in the username input box with a username that exists
     def input_valid_username
         fill_in(USERNAME_INPUT_ID, :with => 'Believe')
     end
 
+    # fills in the password entry with invalid password that is short
     def input_short_password
         fill_in(PASSWORD_INPUT_ID, :with => 'aa12')
     end
 
+    # fills in the password entry with invalid password that uses only letters
     def input_character_only_password
         fill_in(PASSWORD_INPUT_ID, :with => 'aaaaaaaaaa')
     end
 
+    # fills in the password entry with valid password
     def input_acceptable_password
         fill_in(PASSWORD_INPUT_ID, :with => 'qwerty12345')
     end
 
+    # methods that find the error messages on the page, for wrong username, password and general
     def get_general_error_message
         find('#form-message-general').text
     end
